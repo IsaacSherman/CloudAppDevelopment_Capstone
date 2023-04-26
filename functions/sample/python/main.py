d7d400cef28daa7a3566f12bdc58412493f5ace1
IsaacSherman <isaac.b.sherman@gmail.com>
2023-04-26 19:47:45 -0400

    Ugh

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,35 +1,43 @@
+
 """IBM Cloud Function that gets all reviews for a dealership
 
 Returns:
     List: List of reviews for the given dealership
 """
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import requests
+import sys
+d = {
+     "COUCH_URL": "https://apikey-v2-xuso6iwsvgf8lrstj2rdk4sqten31eg8x2fge5erjgj:1f20061c1971f7212c14cc02a90db4c4@506718bd-b1ce-4e22-b680-50edf9440bca-bluemix.cloudantnosqldb.appdomain.cloud",
+    "IAM_API_KEY": "tcsOCvIqxa6jZ-8r-PFzYDwAWCrJ1EbH4-1twofiPipo",
+    "COUCH_USERNAME": "apikey-v2-xuso6iwsvgf8lrstj2rdk4sqten31eg8x2fge5erjgj"
+}
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+
+
 
 
 def main(param_dict):
     """Main Function
 
     Args:
-        param_dict (Dict): input paramater
+        param_dict (Dict): json containing COUCH_URL, IAM_API_KEY, and COUCH_USERNAME
 
     Returns:
-        _type_: _description_ TODO
+        json of database names
     """
 
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
+    authenticator = IAMAuthenticator(param_dict["IAM_API_KEY"])
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url(param_dict["COUCH_URL"])
 
-    return {"dbs": client.all_dbs()}
+    return {"dbs": service.get_all_dbs().get_result()}
