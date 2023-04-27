a1241f4f1156bf4fee1a15ac59ee3e5c28a9a4fe
IsaacSherman <sherman1337@gmail.com>
2023-04-27 08:52:17 -0400

    Update main.py

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -8,11 +8,6 @@ from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import requests
 import sys
-d = {
-     "COUCH_URL": "https://apikey-v2-xuso6iwsvgf8lrstj2rdk4sqten31eg8x2fge5erjgj:1f20061c1971f7212c14cc02a90db4c4@506718bd-b1ce-4e22-b680-50edf9440bca-bluemix.cloudantnosqldb.appdomain.cloud",
-    "IAM_API_KEY": "tcsOCvIqxa6jZ-8r-PFzYDwAWCrJ1EbH4-1twofiPipo",
-    "COUCH_USERNAME": "apikey-v2-xuso6iwsvgf8lrstj2rdk4sqten31eg8x2fge5erjgj"
-}
 #
 #
 # main() will be run when you invoke this action
